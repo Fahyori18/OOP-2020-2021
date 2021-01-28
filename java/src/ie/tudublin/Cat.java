8977236aa5b5bc841946d16415d318996d020c61
unknown <Praifawss18@gmail.com>
2021-01-28 13:21:54 +0000

    killing the cat

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -2,8 +2,32 @@ package ie.tudublin;
 
 public class Cat extends Animal
 {
+    private int numLives;
+
     public Cat(String name)
     {
         super(name);
+        numLives = 9; 
+    }
+
+    public void catKill() {
+
+        if(numLives > 0){
+            
+            System.out.println("Ouch!");
+        }
+        else
+        {
+            System.out.println("I'm Dead!");
+        }
+        numLives--;
+    }
+
+    public int getNumLives() {
+        return numLives;
+    }
+
+    public void setNumLives(int numLives) {
+        this.numLives = numLives;
     }
 }
